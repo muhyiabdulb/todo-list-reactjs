d8c1ca88f8f9af3f46b65db82e209b725d76423a
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2021-08-24 07:23:52 +0700

    ubah jadi class

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import './App.css';
 import React from'react'
 import Todo from './components/todo/Todo';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Todo />
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return(
+      <div className="App">
+        <Todo />
+      </div>
+    )
+  }
 }
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <Todo />
+//     </div>
+//   )
+// }
 
 export default App;
